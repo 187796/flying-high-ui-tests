39f25efba3593f59aae4a78cd3331e7d44ff2133
Krystian Ryniec <187796@edu.p.lodz.pl>
2016-06-28 22:48:39 +0200

    zmiana z Cucumber na CucumberWithSerenity

--- src/test/java/edu/iis/mto/bdd/cucumber/UserAuthenticationTests.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/UserAuthenticationTests.java
@@ -1,10 +1,10 @@
 package edu.iis.mto.bdd.cucumber;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+@RunWith(CucumberWithSerenity.class)
 @CucumberOptions(plugin = { "pretty",
 		"html:target/cucumber" },  tags = {"@authentication"}, glue = "edu.iis.mto.bdd.cucumber.steps")
 public class UserAuthenticationTests {
